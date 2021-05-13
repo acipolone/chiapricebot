94b0af055f81ec9cbb86947e7439dc677c4f1167
Anthony Cipolone <acipolone@gmail.com>
2021-05-13 19:58:02 -0400

    Changes to be committed: 	modified:   chiapricebot.js

--- chiapricebot.js
+++ chiapricebot.js
@@ -1,3 +1,11 @@
+/*
+
+PriceBot - Changes nickname based on crypto price from CoinGecko API
+
+This can be adapted for any crypto by changing the crypto name in config.json to a choice from the coingecko API
+
+*/
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
@@ -23,37 +31,53 @@ bot.on('ready', () => {
     logger.info('Connected');
     logger.info('Logged in as: ' + bot.user.tag + ' (' + bot.user.id + ')');
 
+	// Initial nickname change on startup
 	bot.guilds.cache.map((guild) => {
-	  	var coindata = fetchPriceData(config.coin);
+		var coindata = fetchPriceData(config.coin);
 		coindata.then(function(res) {
 			let priceChange = ((res['market_data']['price_change_24h_in_currency']['usd']+Number.EPSILON)*100)/100;
+			
+			// set the status for price change, adjusting for currency symbol outside of the negative if <0
 			if (priceChange>=0) { var status = '24h +$' + (Math.round((res['market_data']['price_change_24h_in_currency']['usd']+Number.EPSILON)*100)/100); }
 			else { var status = '24h -$' + (Math.round((res['market_data']['price_change_24h_in_currency']['usd']+Number.EPSILON)*-100)/100); };
+			
+			// generate nickname of capitalized symbol and price
 			var username = res['symbol'].toUpperCase() + ' $' + res['market_data']['current_price']['usd'];
+			
+			// set the nickname and the status
 			guild.members.cache.get(bot.user.id).setNickname(username);
 			bot.user.setActivity(status, { type: "WATCHING" });
 		});
-	}); 
-	// update every 60 seconds
-	var dayMillseconds = 1000 * 60;
+	});
 
-    setInterval(function(){ 
+	// update every 60 seconds
+	var updateInterval = 1000 * 60;
+
+    // using updateInterval, get coindata, then update nickname on all guilds/servers that the bot is on
+	setInterval(function(){ 
 		bot.guilds.cache.map((guild) => {
 			var coindata = fetchPriceData(config.coin);
 			coindata.then(function(res) {
 				let priceChange = ((res['market_data']['price_change_24h_in_currency']['usd']+Number.EPSILON)*100)/100;
+				
+				// set the status for price change, adjusting for currency symbol outside of the negative if <0
 				if (priceChange>=0) { var status = '24h +$' + (Math.round((res['market_data']['price_change_24h_in_currency']['usd']+Number.EPSILON)*100)/100); }
 				else { var status = '24h -$' + (Math.round((res['market_data']['price_change_24h_in_currency']['usd']+Number.EPSILON)*-100)/100); };
+				
+				// generate nickname of capitalized symbol and price
 				var username = res['symbol'].toUpperCase() + ' $' + res['market_data']['current_price']['usd'];
+				
+				// set the nickname and the status
 				guild.members.cache.get(bot.user.id).setNickname(username);
 				bot.user.setActivity(status, { type: "WATCHING" });
 			});
 		});
-	}, dayMillseconds);
+	}, updateInterval);
 });
 
 bot.login(auth.token);
 
+// async function to get price data from coingecko API for coin,read from config
 async function fetchPriceData(coin) {
 	var response = await fetch('https://api.coingecko.com/api/v3/coins/'+coin+'?localization=false&community_data=false&developer_data=false&sparkline=false');
 	var data = await response.json();
